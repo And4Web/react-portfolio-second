2d82d04d8c485117ad58e2a6d7f133c6db32c138
And4Web <anands2807@gmail.com>
2023-04-01 11:53:15 +0530

    footer added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import './footer.css'
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <p>Developed and maintained by &#169;And4Web {new Date().getFullYear()}.</p>
+    </div>
   )
 }
 
