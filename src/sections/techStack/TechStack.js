fb5858decbac5e977bbe19182ebfa7c820cf9ac9
And4Web <anands2807@gmail.com>
2023-04-05 17:00:28 +0530

    few changes in animations

--- src/sections/techStack/TechStack.js
+++ src/sections/techStack/TechStack.js
@@ -6,6 +6,11 @@ function TechStack() {
     <div className='services'>
       <h1 className='section-title'>My Stack</h1>
       <div className="line"></div>
+      <p className='stack-desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam est vitae atque labore eius consequuntur quia officiis amet sunt quae eveniet exercitationem cumque tenetur blanditiis, eum quasi laudantium incidunt nulla sed, asperiores distinctio inventore soluta qui nobis. Itaque, ea dolor. Impedit rem optio saepe commodi non cumque laborum natus fuga.</p>
+      <div className="stack-container">
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
     </div>
   )
 }
